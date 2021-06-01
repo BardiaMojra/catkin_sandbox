d4c21a16c5a9c1cba6133e3151612936ac0aad1c
bardi <bardia.mojra@gmail.com>
2021-06-01 10:38:58 -0500

    test build

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/smerx/git/catkin_sandbox/catkin_ws/src
+# Build directory: /home/smerx/git/catkin_sandbox/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("beginner_tutorials")
